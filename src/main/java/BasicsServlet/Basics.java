fd8c2990f73b2585a9e18e03d3248d51f86160af
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-18 22:33:36 +0530

    Basics of Servlet Interface

--- src/main/java/BasicsServlet/Basics.java
+++ src/main/java/BasicsServlet/Basics.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/even")
+@WebServlet("/web-page-link")
 public class Basics extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
