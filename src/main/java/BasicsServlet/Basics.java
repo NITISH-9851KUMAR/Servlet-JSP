12dfb26a1cce821c95d85c921da58568145b3fa2
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-19 17:44:55 +0530

    Commonly Used Servlet HttpServlet

--- src/main/java/BasicsServlet/Basics.java
+++ src/main/java/BasicsServlet/Basics.java
@@ -1,20 +1,19 @@
 package BasicsServlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/web-page-link")
 public class Basics extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         PrintWriter out= response.getWriter();
         response.setContentType("text/html");
-        out.println("<a> Har Har Mahadev! </a>");
+        out.println("<h5><a> Basics HttpServlet </a> </h5>");
+        out.println("<h5><a> Commonly used Servlet </h5></a>");
         out.close();
     }
 }
