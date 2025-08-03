80085158d97810e3f17a92d2868322b3e5ee78cc
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-08-03 12:05:24 +0530

    Print Table Using Custom Tag Handler

---
+++ src/main/java/JSP_Program/PrintTableTag.java
@@ -0,0 +1,32 @@
+package JSP_Program;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class PrintTableTag extends TagSupport {
+
+    int number; // This value fetch by tld Folder which is set by .jsp file
+    String color; // It has also same as above attribute
+
+    public int doStartTag() throws JspException {
+        JspWriter out= pageContext.getOut();
+
+        try{
+
+            out.println("<h3>Table:  "+number+"</h3>");
+            out.println("<div style=' color:"+color+" ' >");
+            for(int i= 1; i<=10; i++){
+                out.println(number*i);
+                out.println("<br>");
+            }
+            out.println("</div>");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return SKIP_BODY;
+    }
+
+}
